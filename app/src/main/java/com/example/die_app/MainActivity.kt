bff49290e844d46d3a62189d283a22fe117be479
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-08-11 15:22:36 -0400

    created shared preferences for storing data

--- app/src/main/java/com/example/die_app/MainActivity.kt
+++ app/src/main/java/com/example/die_app/MainActivity.kt
@@ -1,9 +1,22 @@
 package com.example.die_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    // Shared preferences for storing data
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var resultTextView: TextView
+    private lateinit var secondResultTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
